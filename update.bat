49cc279872f9a7b1d98fd6e13b9a93022ea5fb00
FanHongchuang <847923458@qq.com>
2019-07-06 18:07:18 +0800

    update

--- update.bat
+++ update.bat
@@ -1,8 +1,20 @@
 e:
 cd MyNotebook
-@git pull
-@git add .
-@git commit -m "update"
-@git push origin master
-@mkdocs build
-@mkdocs gh-deploy
+echo pulling ...
+git pull
+echo pull done
+echo add all change into repositories
+git add .
+echo add done
+echo commit all change
+git commit -m "update"
+echo commit done
+echo push it to remote repositories
+git push origin master
+echo push done
+echo convert markdown files to html files
+mkdocs build
+echo convert done
+echo deploying ...
+mkdocs gh-deploy
+echo deploy done
